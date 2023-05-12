036f2f3cd94dd306ad8c5dcd999ed9361a5bd25b
slbabin <>
2023-05-12 20:31:24 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const questions [{
+    question: "When a vehicle is stopped in a tunnel due to congestion what should a driver do?",
+    answer: ["Switch off the engine","Keep the engine ticking over in a low gear","Switch on the fog lights", "Only give hand signals."],
+    correct: "a",
+}
+{
+
+}
+]
